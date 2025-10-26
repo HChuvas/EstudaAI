1985bf86c39ad716bae022c52250b0c79bd49403
Henrique Chuvas <henriquechuvas@hotmail.com>
2025-10-26 19:09:25 -0300

    adiciona mais dependências e os arquivos de server e router

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import { router } from "./router.js"
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin: [],
+    credentials: true
+}))
+app.use(router)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}/`))
