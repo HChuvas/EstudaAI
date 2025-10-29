7eab3962bfad503a9edab76841973a8427529d83
Henrique Chuvas <henriquechuvas@hotmail.com>
2025-10-29 18:22:46 -0300

    adiciona prisma client, conexão com o banco do supabase e uma rota teste de usuários

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
+import { prisma } from "./database/index.js"
 import { Router } from "express"
 
 const router = Router()
 
-
+router.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
 
 export { router }
