78144e2af0d04b3bf72df946060ad5e2ce55669a
Henrique Chuvas <henriquechuvas@hotmail.com>
2025-10-31 20:40:12 -0300

    adicionado serviço de login do usuário

---
+++ src/services/user.service.ts
@@ -0,0 +1,46 @@
+import { prisma } from "../database/index.js";
+import type { Role } from "../types/roles.js";
+
+
+class UserService {
+    async login(email: string, password: string) {
+        const user = await prisma.user.findFirst({
+            where: { email, password },
+            include: {
+                student: true,
+                teacher: true
+            }
+        })
+
+        if(!user) {
+            throw new Error("Credenciais Inválidas")
+        }
+
+        let role: Role
+
+        if(user.student) {
+            role = "STUDENT"
+        }
+
+        if(user.teacher) {
+            role = "TEACHER"
+        }
+
+        else{
+            throw new Error("Unknown user type")
+        }
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role
+        }
+    }
+
+    async register(body) {
+        
+    }
+}
+
+export const userService = new UserService()
