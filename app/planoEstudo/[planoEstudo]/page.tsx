7f5367431bf93cf067464b599acdc6a7b8550114
jaumPedr <jpma306@gmail.com>
2025-11-25 17:52:48 -0300

    plano de Estudos Tela ajustes

--- app/planoEstudo/[planoEstudo]/page.tsx
+++ app/planoEstudo/[planoEstudo]/page.tsx
@@ -12,12 +12,14 @@ type Task = {
     id: number;
     titulo: string;
     conteudo: string;
+    completado?: boolean;
 }
 
 type CheckList = {
     id: number;
     tasks: Task[];
 }
+
 
 export default function Page() {
     
@@ -33,27 +35,43 @@ export default function Page() {
         {
             id: 1,
             titulo: 'Leitura Obrigatória',
-            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen)'
+            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen)',
+            completado: true
         },
         {
-            id: 1,
+            id: 2,
             titulo: 'Leitura Obrigatória',
-            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen)'
+            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen) Ler Capítulo 3 do livro "Introdução a Algoritmos',
+            completado: false
         },
         {
-            id: 1,
+            id: 3,
             titulo: 'Leitura Obrigatória',
-            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen)'
+            conteudo: 'Ler Capítulo 3 do livro "Introdução a Algoritmos" (Cormen)',
+            completado: false
         }
     ]
       
     })
 
+
+    const toggleTaskCompletion = useCallback((taskId: number) => {
+    setCheckList(prev => (
+        {
+        ...prev,
+        tasks: prev.tasks.map(task =>
+            task.id === taskId 
+                ? { ...task, completado: !task.completado }
+                : task
+        )
+    }));
+}, []);
+
     return (
         <main className="flex flex-col w-screen h-screen overflow-hidden bg-[#F5F5F5]">
             
             <div className="flex flex-row  justify-between px-6 py-3 w-screen max-h-[7%] bg-[#098842] drop-shadow ">
-                <h1 className="flex grow justify-center">Plano de Estudos ED</h1>
+                <h1 className="flex grow justify-center">{planoEstudos.titulo}</h1>
                 <button className="w-5 h-5 cursor-pointer">
                     <Image src="/imagens/X-branco.svg" width={16} height={16} alt="Fechar"></Image>
                 </button>
@@ -61,14 +79,26 @@ export default function Page() {
 
             <div className="flex flex-row justify-between min-h-[93%] px-8 py-10 gap-8">
 
-                <div className=" w-[75%] min-h-[85%] px-5 py-5 bg-[#FFFFFF] rounded-lg drop-shadow overflow-auto">
-                    <p className="text-[#494949] whitespace-pre-line">{planoEstudos.conteudo}</p>
+                <div className=" flex flex-row w-[75%] min-h-[85%] bg-[#FFFFFF] rounded-lg drop-shadow">
+                    
+                    <p className=" px-5 py-5 text-[#494949] whitespace-pre-line w-[97%] overflow-auto
+                    
+                    [&::-webkit-scrollbar]:hidden
+                  [-ms-overflow-style:none]
+                  [scrollbar-width:none]"
+                
+                    >{planoEstudos.conteudo}</p>
+                    <div className="flex flex-col justify-end ml-2 mb-2 w-[3%]">
+                        <button className="w-5 h-5 cursor-pointer">
+                            <Image src="/imagens/editar-cinza.svg" width={16} height={16} alt="editar"></Image>
+                        </button>
+                    </div>
             
                 </div>
 
                 <div className=" w-[25%] min-h-[85%] bg-[#FFFFFF] rounded-lg drop-shadow gap-2 overflow-auto">
 
-                    <div className="flex justify-center p-1 m-0.5 font-semibold text-[#686464] border-b border-[#494949]">
+                    <div className="flex justify-center p-1 m-0.5 font-semibold text-[#686464] ">
                         <button className="w-5 h-5 cursor-pointer">
                             <Image src="/imagens/add-cinza.svg" width={16} height={16} alt="adicionar"></Image>
                         </button>
@@ -77,11 +107,13 @@ export default function Page() {
 
                     {checkList.tasks.map( task=>(
                         
-                        <div className="flex flex-row ">
-                            <input className="m-4 w-7 h-7" type="checkbox" />
+                        <div key={task.id}className = {  ` flex flex-row ${task.completado ? 'bg-[#E3E3E3] line-through decoration-[#686464]' : '' } ` }  >
+                            <button className="m-4 cursor-pointer flex-none" onClick={() => toggleTaskCompletion(task.id)}>
+                                <Image src={ task.completado ? '/imagens/check_box_marcada.svg' : '/imagens/check_box.svg' } width={18} height={18} alt = 'checkbox'></Image>
+                            </button>
                             
-                            <div className="flex shrink flex-col max-h-[65%] justify-center">
-                                <h3 className="  text-[#686464] font-semibold text-sm">{task.titulo}</h3>
+                            <div className="flex flex-col max-h-[65%] justify-center">
+                                <h3 className="text-[#686464] font-semibold text-sm">{task.titulo}</h3>
                                 <p className=" text-[#686464] text-xs">{task.conteudo}</p>
                             </div>
 
